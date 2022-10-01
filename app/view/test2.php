0f8722f6dd7214a1216c3167c6e9b64ca9a8f015
Elvis Leite <elvis7t@gmail.com>
2022-10-01 20:02:36 -0300

    Test

--- app/view/test2.php
+++ app/view/test2.php
@@ -1,6 +1,19 @@
 <?php
 require_once('../config/authentica.php');
 ?>
+<?php
+require_once('../config/authentica.php');
+
+$Url[1] = (empty($Url[1]) ? null : $Url[1]);
+if(file_exists(DIRECTORY_SEPARATOR . '/' .$Url[0] . 'php' )):
+    require DIRECTORY_SEPARATOR . '/' .$Url[0] . 'php';
+elseif(file_exists(DIRECTORY_SEPARATOR . '/' . $Url[0] . '/' . $Url[1] . 'php')):  
+    require DIRECTORY_SEPARATOR . '/' . $Url[0] . '/' . $Url[1] . 'php';
+else:
+        require '../404.php';  
+endif;
+require_once('../config/footer.php');
+?>
 <body>
     <div class="d-flex justify-content-center">
         <h1>
